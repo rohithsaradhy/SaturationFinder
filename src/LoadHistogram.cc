baddca82e57594787da352a647a66fd953ccdc6a
rohithsaradhy <rohithsaradhy@gmail.com>
2017-12-31 04:08:25 -0600

    Some printing statements improvements

--- src/LoadHistogram.cc
+++ src/LoadHistogram.cc
@@ -6,7 +6,7 @@ void SaturationFinder::LoadHistogram()
   std::ostringstream os( std::ostringstream::ate );
 
   filename.str("");
-  filename<<"./temp_data/"<<ENERGY<<"_"<<RUN_TYPE<<".root";
+  filename<<"./temp_data/"<<FIT_NAME<<"_Energy_"<<ENERGY<<"_"<<RUN_TYPE<<".root";
 
   TH2F* temp;
   TH2F* temp1;
@@ -18,19 +18,20 @@ void SaturationFinder::LoadHistogram()
     for(int j=0;j<4;j++)
     {
       os.str("");
-      os<<ENERGY<<"_Board_"<<i<<"_Skiroc_"<<j<<"_HighGaintoLowGain";
+      os<<FIT_NAME<<"_Energy_"<<ENERGY<<"_Board_"<<i<<"_Skiroc_"<<j<<"_HighGaintoLowGain";
       gDirectory->GetObject(os.str().c_str(),Hist2D[i][j][0]);
       Hist2D[i][j][0]->SetDirectory(0) ; //Keep the Hist stay after file close...
       os.str("");
-      os<<ENERGY<<"_Board_"<<i<<"_Skiroc_"<<j<<"_LowGaintoTOTSLow";
+      os<<FIT_NAME<<"_Energy_"<<ENERGY<<"_Board_"<<i<<"_Skiroc_"<<j<<"_LowGaintoTOTSLow";
       gDirectory->GetObject(os.str().c_str(),Hist2D[i][j][1] );
       Hist2D[i][j][1]->SetDirectory(0) ;
 
-      std::cout<<ENERGY<<"\t"<<RUN_TYPE<<"\t"<<"Loading... \t "<<(i*4 + j)*100/(BOARD*4 + 3)<<"\r";
+      std::cout<<FIT_NAME<<"_Energy_"<<ENERGY<<"_"<<RUN_TYPE<<"\t"<<"Loading :: "<<(i*4 + j)*100/(BOARD*4 + 3)<<"% \r";
       std::cout.flush();
 
     }
   }
+  std::cout<<std::endl;
 
   file->Close();
 }
--- src/SaveHistogram.cc
+++ src/SaveHistogram.cc
@@ -8,7 +8,7 @@ void SaturationFinder::SaveHistogram()
   std::ostringstream os( std::ostringstream::ate );
 
   filename.str("");
-  filename<<"./temp_data/"<<ENERGY<<"_"<<RUN_TYPE<<".root";
+  filename<<"./temp_data/"<<FIT_NAME<<"_Energy_"<<ENERGY<<"_"<<RUN_TYPE<<".root";
 
   TFile *file = TFile::Open(filename.str().c_str(),"RECREATE");
   for(int i=0;i<BOARD;i++)
@@ -16,17 +16,18 @@ void SaturationFinder::SaveHistogram()
     for(int j=0;j<4;j++)
     {
           os.str("");
-          os<<ENERGY<<"_Board_"<<i<<"_Skiroc_"<<j<<"_HighGaintoLowGain";
+          os<<FIT_NAME<<"_Energy_"<<ENERGY<<"_Board_"<<i<<"_Skiroc_"<<j<<"_HighGaintoLowGain";
           Hist2D[i][j][0]->Write(os.str().c_str());
           os.str("");
-          os<<ENERGY<<"_Board_"<<i<<"_Skiroc_"<<j<<"_LowGaintoTOTSLow";
+          os<<FIT_NAME<<"_Energy_"<<ENERGY<<"_Board_"<<i<<"_Skiroc_"<<j<<"_LowGaintoTOTSLow";
           Hist2D[i][j][1]->Write(os.str().c_str());
 
-          std::cout<<ENERGY<<"\t"<<RUN_TYPE<<"\t"<<"Saving... \t "<<(i*4 + j)*100/(BOARD*4 + 3)<<"\r";
+          std::cout<<FIT_NAME<<"_Energy_"<<ENERGY<<"_"<<RUN_TYPE<<"\t"<<"Saving  :: "<<(i*4 + j)*100/(BOARD*4 + 3)<<"% \r";
           std::cout.flush();
 
     }
   }
+  std::cout<<std::endl;
 
   file->Close();
 
