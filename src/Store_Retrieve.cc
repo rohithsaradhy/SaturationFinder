5d0f9de36fdbb94e6f181f7a2c3d0c37a2437a22
rohithsaradhy <rohithsaradhy@gmail.com>
2018-01-04 05:53:16 -0600

    Retreive added

--- src/Store_Retrieve.cc
+++ src/Store_Retrieve.cc
@@ -8,7 +8,7 @@ void SaturationFinder::StoreValues(std::string rootFolder)
   std::ostringstream os( std::ostringstream::ate );
   std::ofstream fs;
   os.str("");
-  os<<rootFolder<<"HG_LG_Datbase.txt";
+  os<<rootFolder<<FIT_NAME<<"_HG_LG_Datbase.txt";
   fs.open(os.str().c_str());
   fs<<std::fixed;
   int type_num =0;
@@ -28,7 +28,7 @@ void SaturationFinder::StoreValues(std::string rootFolder)
 
 
   os.str("");
-  os<<rootFolder<<"LG_TOT_Datbase.txt";
+  os<<rootFolder<<FIT_NAME<<"_LG_TOT_Datbase.txt";
   fs.open (os.str().c_str());
   fs<<std::fixed;
   type_num =1;
@@ -51,5 +51,56 @@ void SaturationFinder::StoreValues(std::string rootFolder)
 void SaturationFinder::RetrieveValues(std::string rootFolder)
 {
 
+    std::ostringstream os( std::ostringstream::ate );
+    int brd,ski,type_num;
+    float cf,cf_e,cf_int,cf_int_e,tp,tp_e;
+    std::fstream fs;
 
+
+    type_num =0;
+    os.str("");
+    os<<rootFolder<<FIT_NAME<<"_HG_LG_Datbase.txt";
+    fs.open (os.str().c_str(), std::fstream::in | std::fstream::out | std::fstream::app);
+    while(!fs.eof())
+    {
+
+      fs>>brd>>ski>>cf>>cf_e>>cf_int>>cf_int_e>>tp>>tp_e;
+      // std::cout<<brd<<"\t"<<ski<<"\t"<<fs.eof()<<std::endl;
+
+      CF[brd][ski][type_num] = cf;
+      CF_Err[brd][ski][type_num] = cf_e;
+      CF_Intercept[brd][ski][type_num] = cf_int;
+      CF_Intercept_Err[brd][ski][type_num] = cf_int_e;
+      TP[brd][ski][type_num] = tp;
+      TP_Err[brd][ski][type_num] = tp_e;
+
+
+
+    }
+    std::cout<<"Values have been retrieved into the arrays for HG_LG"<<std::endl;
+    fs.close();
+
+
+    type_num =1;
+    os.str("");
+    os<<rootFolder<<FIT_NAME<<"_LG_TOT_Datbase.txt";
+    fs.open (os.str().c_str(), std::fstream::in | std::fstream::out | std::fstream::app);
+    while(!fs.eof())
+    {
+
+      fs>>brd>>ski>>cf>>cf_e>>cf_int>>cf_int_e>>tp>>tp_e;
+      // std::cout<<brd<<"\t"<<ski<<"\t"<<fs.eof()<<std::endl;
+
+      CF[brd][ski][type_num] = cf;
+      CF_Err[brd][ski][type_num] = cf_e;
+      CF_Intercept[brd][ski][type_num] = cf_int;
+      CF_Intercept_Err[brd][ski][type_num] = cf_int_e;
+      TP[brd][ski][type_num] = tp;
+      TP_Err[brd][ski][type_num] = tp_e;
+
+
+
+    }
+    std::cout<<"Values have been retrieved into the arrays for LG_TOT"<<std::endl;
+    fs.close();
 }
